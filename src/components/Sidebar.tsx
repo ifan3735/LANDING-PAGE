1d9c69687a19b631202a4891d1b735fa06e9cf8d
ifan3735 <ifanmaina@gmail.com>
2024-09-08 14:30:32 +0300

    46 commit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -20,37 +20,86 @@ const Sidebar = () => {
       
       <ul className="flex-1 space-y-2 px-4">
         <li>
-          <NavLink to="/dashboard" className="flex items-center p-4 hover:bg-yellow-500 rounded-md">
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) =>
+              `flex items-center p-4 rounded-md ${
+                isActive ? 'bg-yellow-500' : 'hover:bg-yellow-500'
+              }`
+            }
+          >
             <FaCar className="mr-3" /> Dashboard
           </NavLink>
         </li>
         <li>
-          <NavLink to="/listings" className="flex items-center p-4 hover:bg-yellow-500 rounded-md">
+          <NavLink
+            to="/listings"
+            className={({ isActive }) =>
+              `flex items-center p-4 rounded-md ${
+                isActive ? 'bg-yellow-500' : 'hover:bg-yellow-500'
+              }`
+            }
+          >
             <FaList className="mr-3" /> Listings
           </NavLink>
         </li>
         <li>
-          <NavLink to="/tracking" className="flex items-center p-4 hover:bg-yellow-500 rounded-md">
+          <NavLink
+            to="/tracking"
+            className={({ isActive }) =>
+              `flex items-center p-4 rounded-md ${
+                isActive ? 'bg-yellow-500' : 'hover:bg-yellow-500'
+              }`
+            }
+          >
             <FaMapMarkerAlt className="mr-3" /> Tracking
           </NavLink>
         </li>
         <li>
-          <NavLink to="/bids" className="flex items-center p-4 hover:bg-yellow-500 rounded-md">
+          <NavLink
+            to="/bids"
+            className={({ isActive }) =>
+              `flex items-center p-4 rounded-md ${
+                isActive ? 'bg-yellow-500' : 'hover:bg-yellow-500'
+              }`
+            }
+          >
             <FaDollarSign className="mr-3" /> Bids
           </NavLink>
         </li>
         <li>
-          <NavLink to="/calendar" className="flex items-center p-4 hover:bg-yellow-500 rounded-md">
+          <NavLink
+            to="/calendar"
+            className={({ isActive }) =>
+              `flex items-center p-4 rounded-md ${
+                isActive ? 'bg-yellow-500' : 'hover:bg-yellow-500'
+              }`
+            }
+          >
             <FaCalendarAlt className="mr-3" /> Calendar
           </NavLink>
         </li>
         <li>
-          <NavLink to="/transactions" className="flex items-center p-4 hover:bg-yellow-500 rounded-md">
+          <NavLink
+            to="/transactions"
+            className={({ isActive }) =>
+              `flex items-center p-4 rounded-md ${
+                isActive ? 'bg-yellow-500' : 'hover:bg-yellow-500'
+              }`
+            }
+          >
             <FaDollarSign className="mr-3" /> Transactions
           </NavLink>
         </li>
         <li>
-          <NavLink to="/settings" className="flex items-center p-4 hover:bg-yellow-500 rounded-md">
+          <NavLink
+            to="/settings"
+            className={({ isActive }) =>
+              `flex items-center p-4 rounded-md ${
+                isActive ? 'bg-yellow-500' : 'hover:bg-yellow-500'
+              }`
+            }
+          >
             <FaCog className="mr-3" /> Settings
           </NavLink>
         </li>
