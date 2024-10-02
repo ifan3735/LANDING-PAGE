f83651fead6c3a31ada81c33a423a501fd70f67a
ifan3735 <ifanmaina@gmail.com>
2024-10-02 20:24:18 +0300

    150 commit

--- src/pages/Deals.tsx
+++ src/pages/Deals.tsx
@@ -1,4 +1,15 @@
+import TopBar from "../components/TopBar";
+import { useState } from "react";
+
 const Deals = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [theme, setTheme] = useState("light");
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedFilters, setSelectedFilters] = useState({
+        type: '',
+        color: '',
+        style: '',
+      });
     return (
         <div>
         <h1>Deals</h1>
