a392f186ef4d8e4f2568296c819fda19faf4bf43
ifan3735 <ifanmaina@gmail.com>
2024-10-03 19:36:16 +0300

    163 commit

--- src/pages/Deals.tsx
+++ src/pages/Deals.tsx
@@ -185,7 +185,7 @@ const Deals = () => {
                 key={deal.id}
                 className="border-b border-gray-200 hover:bg-gray-100"
               >
-                <td className="py-3 px-6">{index + 1}</td>
+                <td className="py-3 px-6">{deal.id}</td>
                 <td className="py-3 px-6 flex items-center">
                   <img
                     src={deal.profileImage}
