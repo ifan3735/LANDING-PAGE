2053b9cf1be726326705ded6d7e0c0645ae665f9
ifan3735 <ifanmaina@gmail.com>
2024-10-05 16:53:00 +0300

    188 commit

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
+import TopBar from "../components/TopBar";
+
 const Settings = () => {
   const [showDropdown, setShowDropdown] = useState(false); // To toggle export dropdown
   const [theme, setTheme] = useState('light');
   const [searchQuery, setSearchQuery] = useState('');
-  const [showFilterDropdown, setShowFilterDropdown] = useState(false); // For showing/hiding filter dropdown
   const [selectedFilters, setSelectedFilters] = useState({
     type: '',
     color: '',
     style: '',
   });
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'yellow' : 'light');
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value);
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setSelectedFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value,
+    }));
+  };
     return (
-      <div>
-        <h2 className="text-2xl font-semibold mb-4">Settings</h2>
-        <p>Settings options go here...</p>
+      <div className={`transition-all duration-500 p-6 ${theme === 'yellow' ? 'bg-yellow-100 text-gray-900' : 'bg-gray-100 text-gray-900'} min-h-screen`}>
+         <TopBar
+        searchQuery={searchQuery}
+        handleSearch={handleSearch}
+        toggleTheme={toggleTheme}
+        theme={theme}
+        exportData={() => exportData(filteredCars)}
+      />
       </div>
     );
   };
