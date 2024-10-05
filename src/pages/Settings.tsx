628ca264113fe20869420b1d275cf4c4887454e8
ifan3735 <ifanmaina@gmail.com>
2024-10-05 16:50:06 +0300

    187 commit

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,4 +1,13 @@
 const Settings = () => {
+  const [showDropdown, setShowDropdown] = useState(false); // To toggle export dropdown
+  const [theme, setTheme] = useState('light');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showFilterDropdown, setShowFilterDropdown] = useState(false); // For showing/hiding filter dropdown
+  const [selectedFilters, setSelectedFilters] = useState({
+    type: '',
+    color: '',
+    style: '',
+  });
     return (
       <div>
         <h2 className="text-2xl font-semibold mb-4">Settings</h2>
