82deabc6362eaf6a0ca7882b9514dc5a515933c4
ifan3735 <ifanmaina@gmail.com>
2024-10-08 17:46:02 +0300

    230 commit

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import TopBar from "../components/TopBar";
+
 const Help = () => {
+    const [showDropdown, setShowDropdown] = useState(false); // To toggle export dropdown
+  const [theme, setTheme] = useState('light');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showFilterDropdown, setShowFilterDropdown] = useState(false); // For showing/hiding filter dropdown
+  const [selectedFilters, setSelectedFilters] = useState({
+    type: '',
+    color: '',
+    style: '',
+  });
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'yellow' : 'light');
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value);
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setSelectedFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value,
+    }));
+    const toggleFilterDropdown = () => setShowFilterDropdown(!showFilterDropdown);
+    const toggleExportDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+  };
+
     return (
-        <div>
-            <h1>Help</h1>
-            <p>Help content</p>
+        <div className={`transition-all duration-500 p-6 ${theme === 'yellow' ? 'bg-yellow-100 text-gray-900' : 'bg-gray-100 text-gray-900'} min-h-screen`}>
+            <TopBar
+        searchQuery={searchQuery}
+        handleSearch={handleSearch}
+        toggleTheme={toggleTheme}
+        theme={theme}
+        exportData={() => exportData(filteredCars)}
+      />
         </div>
     );
 }
