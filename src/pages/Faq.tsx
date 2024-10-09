71607709f520dda20f6892b4dde966918edaf143
ifan3735 <ifanmaina@gmail.com>
2024-10-09 15:59:41 +0300

    245 commit

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -1,7 +1,12 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 const Faq = () => {
     return (
         <div>
+            <Header />
         <h1>Frequently Asked Questions</h1>
+        <Footer />
         </div>
     );
     };
