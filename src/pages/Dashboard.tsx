d24c75e9135a9c1bb31442687d191cdb67120614
ifan3735 <ifanmaina@gmail.com>
2024-10-15 15:22:41 +0300

    341 commit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -20,15 +20,6 @@ const Dashboard = () => {
   });
   const [visibleActivities, setVisibleActivities] = useState(1);
 
-  // Car Data
-  const cars = [
-    { name: 'Audi R8 Green', style: 'Audi', type: 'Auto', color: 'Green', price: '$285,892', imageUrl: 'https://media.istockphoto.com/id/174691804/photo/green-supercar-isolated.jpg?s=612x612&w=0&k=20&c=nh6aondlx41IkaHJRG9Ffp6CYjGBOTIUCPrVC0Mn4l0=' },
-    { name: 'Bentley Flying Spur', style: 'Bentley', type: 'Petrol', color: 'Brown', price: '$358,174', imageUrl: 'https://i.pinimg.com/1200x/a5/cb/83/a5cb831ea2e399e2e4ede6eab618a4f0.jpg' },
-    { name: 'Lamborghini Aventador', style: 'Lamborghini', type: 'Auto', color: 'Yellow', price: '$420,000', imageUrl: 'https://i.pinimg.com/1200x/66/ee/a9/66eea93dbd62e9d9f65303efe3da1352.jpg' },
-    { name: 'Porsche 911 Turbo', style: 'Porsche', type: 'Auto', color: 'Red', price: '$200,000', imageUrl: 'https://i.pinimg.com/1200x/e1/06/07/e1060739e7e16b2f935ec364c77cddcd.jpg' },
-    { name: 'BMW M5', style: 'BMW', type: 'Manual', color: 'Blue', price: '$110,000', imageUrl: 'https://i.pinimg.com/1200x/e8/f3/29/e8f329616ed58ee086b10112c8a6d157.jpg' },
-    { name: 'Tesla Model S', style: 'Tesla', type: 'Electric', color: 'White', price: '$120,000', imageUrl: 'https://i.pinimg.com/1200x/ba/fa/a9/bafaa9ee834ac42aaf6f08313e930cbe.jpg' },
-  ];
 
   const toggleTheme = () => setTheme(theme === 'light' ? 'yellow' : 'light');
 
@@ -208,11 +199,11 @@ const Dashboard = () => {
               {filteredCars.length ? filteredCars.map((Vehicle, index) => (
                 <CarCard
                   key={index}
-                  name={car.name}
-                  style={car.style}
-                  type={car.type}
-                  color={car.color}
-                  price={car.price}
+                  name={Vehicle.vehicle_specs.model}
+                  style={Vehicle.vehicle_specs.manufacturer}
+                  type={Vehicle.type}
+                  color={Vehicle.color}
+                  price={Vehicle.price}
                   imageUrl={Vehicle.image}
                 />
               )) : (
