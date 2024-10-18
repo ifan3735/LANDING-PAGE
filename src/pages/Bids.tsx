c4e3d4aa47c1b9f4cd7d4033c5ca3178791516d4
ifan3735 <ifanmaina@gmail.com>
2024-10-18 16:27:01 +0300

    439 commit

--- src/pages/Bids.tsx
+++ src/pages/Bids.tsx
@@ -287,7 +287,7 @@ const Bids = () => {
                 </li>
                 <li
                   className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                  onClick={() => console.log("Export as PDF")}
+                  onClick={exportAsPDF}
                 >
                   Export as PDF
                 </li>
