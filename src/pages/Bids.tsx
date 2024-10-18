1f4c4ad8dc3fc2014e72e4937c7e4dc6673cbe6c
ifan3735 <ifanmaina@gmail.com>
2024-10-18 16:25:49 +0300

    438 commit

--- src/pages/Bids.tsx
+++ src/pages/Bids.tsx
@@ -281,7 +281,7 @@ const Bids = () => {
               <ul>
                 <li
                   className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                  onClick={() => console.log("Export as CSV")}
+                  onClick={exportAsCSV}
                 >
                   Export as CSV
                 </li>
