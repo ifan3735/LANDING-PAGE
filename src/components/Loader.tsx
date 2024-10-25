524425ed238d177f002b0d84fac4f990a1492807
ifan3735 <ifanmaina@gmail.com>
2024-10-25 17:24:26 +0300

    509 commit

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,11 +2,20 @@
 import React from 'react';
 
 const Loader: React.FC = () => {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-16 h-16 border-4 border-blue-400 border-solid border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-};
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-50">
+          <div className="relative w-20 h-20 animate-spin-slow">
+            {/* Outer Ring */}
+            <div className="absolute inset-0 rounded-full border-4 border-t-blue-500 border-r-transparent border-b-transparent border-l-blue-500 animate-spin-fast"></div>
+            
+            {/* Middle Ring */}
+            <div className="absolute inset-1 w-16 h-16 rounded-full border-4 border-t-yellow-500 border-r-transparent border-b-transparent border-l-yellow-500 animate-spin-reverse"></div>
+            
+            {/* Inner Ring */}
+            <div className="absolute inset-2 w-12 h-12 rounded-full border-4 border-t-green-500 border-r-transparent border-b-transparent border-l-green-500 animate-spin"></div>
+          </div>
+        </div>
+      );
+    };
 
 export default Loader;
