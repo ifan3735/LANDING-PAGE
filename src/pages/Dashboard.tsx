2aa3d6b088bfc11e629bbae610dfbb8a9dbfd7c1
ifan3735 <ifanmaina@gmail.com>
2024-11-04 10:20:09 +0300

    634 commit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -136,8 +136,8 @@ const Dashboard = () => {
           </p>
         </div>
 
-        <div className="relative">
-          <button
+        <div className="relative z-20"> {/* Increased z-index for the dropdown container */}
+        <button
             onClick={toggleExportDropdown}
             className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full shadow-lg flex items-center"
           >
