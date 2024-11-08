f5cdf8346416eb35337676e0214ce125e5d6fb8b
ifan3735 <ifanmaina@gmail.com>
2024-11-08 17:56:57 +0300

    688 commit

--- src/components/CarTrackingList.tsx
+++ src/components/CarTrackingList.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CarCard from './CarCards';
 import { useFetchAllBookingsQuery } from '../features/API';
+import CarDetails from './CarDetails';
 
 const CarTrackingList: React.FC = () => {
   const { data, error, isLoading } = useFetchAllBookingsQuery();
   console.log(data);
+  const [selectedCar, setSelectedCar] = useState<any>(null); // State to store selected car details
+
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading data</div>;
@@ -34,6 +37,7 @@ const CarTrackingList: React.FC = () => {
             : `${calculateDurationInDays(vehicle.booking_date, vehicle.return_date)} days`;
 
           return (
+            <div onClick={() => setSelectedCar(vehicle)} key={vehicle.id}>
             <CarCard
               key={vehicle.id} // Assuming each booking has a unique `id`
               imageUrl={vehicle.vehicle.image} // Replace with correct property from the API data
@@ -43,8 +47,20 @@ const CarTrackingList: React.FC = () => {
               location={vehicle.location || "Nairobi CBD"} // Replace with correct property from the API data
               duration={duration} // Display "Bought by User" or calculated duration in days
             />
+          </div>
           );
         })}
+      </div>
+      {/* Car Details - displays details of the selected car */}
+      <div className="col-span-8">
+        {selectedCar ? (
+          <CarDetails
+            carName={selectedCar.vehicle.vehicle_specs.manufacturer}
+            imageUrl={selectedCar.vehicle.image}
+          />
+        ) : (
+          <div>Select a car to view details</div>
+        )}
       </div>
     </div>
   );
