cbfd43d79255aecb2c413bd927fd8f525fbb7b14
ifan3735 <ifanmaina@gmail.com>
2024-11-08 18:22:53 +0300

    697 commit

--- src/components/CarDetails.tsx
+++ src/components/CarDetails.tsx
@@ -1,7 +1,7 @@
 interface CarDetailsProps {
   carName: string;
   imageUrl: string;
-  features: string[]; // Update features to be an array of strings
+  features: string; // Update features to be an array of strings
 }
 
 const CarDetails: React.FC<CarDetailsProps> = ({ carName, imageUrl, features }) => {
@@ -16,9 +16,7 @@ const CarDetails: React.FC<CarDetailsProps> = ({ carName, imageUrl, features }) 
         
         {/* Display features list */}
         <ul className="mt-4 text-sm text-gray-600 list-disc list-inside">
-          {features.map((feature, index) => (
-            <li key={index}>{feature}</li>
-          ))}
+          {features}
         </ul>
       </div>
 
